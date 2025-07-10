43ea886782a240bc69fb51b9dae3a5a94b2690db
wlisboy <2042065156@qq.com>
2025-07-10 15:04:04 +0800

    push

--- main.py
+++ main.py
@@ -2586,16 +2586,18 @@ class NewsAnalyzer:
 
         ids = [
             ("toutiao", "今日头条"),
-            ("baidu", "百度热搜"),
-            ("wallstreetcn-hot", "华尔街见闻"),
+            # ("baidu", "百度热搜"),
+            # ("wallstreetcn-hot", "华尔街见闻"),
             ("thepaper", "澎湃新闻"),
             ("bilibili-hot-search", "bilibili 热搜"),
-            ("cls-hot", "财联社热门"),
-            ("ifeng", "凤凰网"),
-            "tieba",
-            "weibo",
-            "douyin",
-            "zhihu",
+            # ("cls-hot", "财联社热门"),
+            # ("ifeng", "凤凰网"),
+            # "tieba",
+            ("weibo","微博"),
+            ("douyin","抖音"),
+            ("zhihu","知乎"),
+            ("coolapk","酷安"),
+            ("github","github")
         ]
 
         print(f"开始爬取数据，请求间隔 {self.request_interval} 毫秒")
